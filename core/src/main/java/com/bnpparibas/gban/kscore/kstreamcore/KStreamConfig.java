f83d7057b29fa222ce007b288d71dededc208878
limitium <13qeadzc@WSX>
2022-10-11 19:50:13 +0100

    Store topics

--- core/src/main/java/com/bnpparibas/gban/kscore/kstreamcore/KStreamConfig.java
+++ core/src/main/java/com/bnpparibas/gban/kscore/kstreamcore/KStreamConfig.java
@@ -1,6 +1,7 @@
 package com.bnpparibas.gban.kscore.kstreamcore;
 
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.processor.internals.ProcessorStateManager;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +10,6 @@ import org.springframework.kafka.annotation.EnableKafkaStreams;
 import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
 import org.springframework.kafka.config.KafkaStreamsConfiguration;
 import org.springframework.kafka.config.KafkaStreamsInfrastructureCustomizer;
-import org.springframework.kafka.config.StreamsBuilderFactoryBean;
 import org.springframework.kafka.config.StreamsBuilderFactoryBeanConfigurer;
 
 import java.util.Map;
@@ -22,19 +22,28 @@ public class KStreamConfig {
     @Value("${kafka.bootstrap.servers}")
     private String kafkaBootStrapServers;
 
+    /**
+     * Sets kafka client configuration
+     *
+     * @param appName
+     * @param kafkaStorePrefix used in {@link ProcessorStateManager#changelogFor(String)}
+     * @return
+     */
     @Bean(name =
             KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
     public KafkaStreamsConfiguration kStreamsConfigs(
-            @Value("${spring.application.name}") String appName
+            @Value("${spring.application.name}") String appName,
+            @Value("${kafka.store.prefix:#{null}}") String kafkaStorePrefix
     ) {
         return new KafkaStreamsConfiguration(Map.of(
                 StreamsConfig.APPLICATION_ID_CONFIG, appName,
-                StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBootStrapServers
+                StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBootStrapServers,
+                StreamsConfig.InternalConfig.TOPIC_PREFIX_ALTERNATIVE, kafkaStorePrefix != null ? kafkaStorePrefix + "." + appName.toLowerCase() + ".store" : appName
         ));
     }
 
     @Bean
-    public StreamsBuilderFactoryBeanConfigurer kStreamsFactoryConfigurer(KafkaStreamsInfrastructureCustomizer topologyProvider){
+    public StreamsBuilderFactoryBeanConfigurer kStreamsFactoryConfigurer(KafkaStreamsInfrastructureCustomizer topologyProvider) {
         return factoryBean -> factoryBean.setInfrastructureCustomizer(topologyProvider);
     }
 }
