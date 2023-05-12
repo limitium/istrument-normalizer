6d7acb6e23fe03745884af62fd6400115bfb705c
limitium <13qeadzc@WSX>
2023-05-12 17:52:47 +0100

    opcode parser

--- communication/src/main/java/com/bnpparibas/gban/flatbufferstooling/communication/NormalizeInstrument.java
+++ communication/src/main/java/com/bnpparibas/gban/flatbufferstooling/communication/NormalizeInstrument.java
@@ -3,7 +3,12 @@ package com.bnpparibas.gban.flatbufferstooling.communication;
 import com.bnpparibas.gban.communication.messages.internal.instrumentnormalizer.receiver.flatbuffers.FBNormalizeInstrument;
 import com.google.flatbuffers.FlatBufferBuilder;
 import com.google.flatbuffers.Table;
+import org.apache.bcel.Repository;
+import org.apache.bcel.classfile.ConstantPool;
+import org.apache.bcel.classfile.JavaClass;
+import org.apache.bcel.generic.*;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.nio.ByteBuffer;
@@ -50,57 +55,77 @@ public class NormalizeInstrument {
 
         int securityIdOff = flatBufferBuilder.createString(securityId);
         int egressTopicOff = flatBufferBuilder.createString(egressTopic);
-        int pathToInstrumentTableOff = flatBufferBuilder.createString(pathToInstrumentTable);
+        int instrumentIdOffset = getInstrumentIdOffset(fbTable, pathToInstrumentTable);
         int msgClazzOff = flatBufferBuilder.createString(fbTable.getClass().getName());
         int msgDataOff = flatBufferBuilder.createByteVector(fbTable.getByteBuffer().array());
 
-        flatBufferBuilder.finish(FBNormalizeInstrument.createFBNormalizeInstrument(flatBufferBuilder, securityIdOff, msgClazzOff, msgDataOff, msgId, pathToInstrumentTableOff, egressTopicOff));
+        flatBufferBuilder.finish(FBNormalizeInstrument.createFBNormalizeInstrument(flatBufferBuilder, securityIdOff, msgClazzOff, msgDataOff, msgId, instrumentIdOffset, egressTopicOff));
 
         return FBNormalizeInstrument.getRootAsFBNormalizeInstrument(ByteBuffer.wrap(flatBufferBuilder.sizedByteArray()));
     }
 
+    private static int getInstrumentIdOffset(Table originalMessage, String pathToInstrumentTable) {
+        Table instrumentTable = (Table) findInstrumentTable(originalMessage, pathToInstrumentTable);
+        return getOffsetForField(instrumentTable, MUTATE_INSTRUMENT_ID);
+    }
+
+    private static int getOffsetForField(Table table, String mutatorName) {
+        try {
+            //VTable offset in bb
+            Field bbPosField = table.getClass().getSuperclass().getDeclaredField("bb_pos");
+            bbPosField.setAccessible(true);
+            Integer bbPos = (Integer) bbPosField.get(table);
+
+            //Field reference offset in vtable
+            int fieldOffset = lookupFieldOffsetInVtable(table.getClass(), mutatorName);
+
+            //Field value offset in vtable
+            java.lang.reflect.Method offsetMethod = table.getClass().getSuperclass().getDeclaredMethod("__offset", int.class);
+            offsetMethod.setAccessible(true);
+            int offset = (int) offsetMethod.invoke(table, fieldOffset);
+
+            return bbPos + offset;
+        } catch (NoSuchFieldException | ClassNotFoundException | IllegalAccessException | NoSuchMethodException |
+                 InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static int lookupFieldOffsetInVtable(Class<? extends Table> tableClass, String mutatorName) throws ClassNotFoundException {
+        JavaClass jc = Repository.lookupClass(tableClass.getName());
+
+        ConstantPool constantPool = jc.getConstantPool();
+        org.apache.bcel.classfile.Method[] method = jc.getMethods();
+        ConstantPoolGen cpg = new ConstantPoolGen(constantPool);
+
+        for (org.apache.bcel.classfile.Method m : method) {
+            if (m.getName().equalsIgnoreCase(mutatorName)) {
+                MethodGen mg = new MethodGen(m, m.getName(), cpg);
+                for (InstructionHandle ih = mg.getInstructionList().getStart();
+                     ih != null; ih = ih.getNext()) {
+                    if (ih.getPosition() == 1) {
+                        return switch (ih.getInstruction()) {
+                            case BIPUSH bipush -> bipush.getValue().intValue();
+                            case ICONST iconst -> iconst.getValue().intValue();
+                            default ->
+                                    throw new RuntimeException("Unsupported 1st opcode" + ih.getInstruction() + "in " + tableClass + "#" + mutatorName);
+                        };
+                    }
+                }
+            }
+        }
+        throw new RuntimeException("Offset wasn't found in " + tableClass + "#" + mutatorName);
+    }
+
     /**
-     * Mutates instrument id in a table of the original message according to {@link FBNormalizeInstrument#pathToInstrumentTable()}
+     * Mutates instrument id in a table of the original message according to {@link FBNormalizeInstrument#instrumentIdOffsest()} ()}
      *
      * @param normalizeInstrument request with original flatbuffers message to be mutated
      * @param instrumentId        public instrument id to be set in the original message
      */
     public static void mutateInstrumentId(FBNormalizeInstrument normalizeInstrument, long instrumentId) {
-        try {
-            Table originalMessage = getOriginalTable(normalizeInstrument);
-
-            Object instrumentTable = findInstrumentTable(originalMessage, normalizeInstrument.pathToInstrumentTable());
-            Method mutator = findMutator(instrumentTable);
-
-            if (!(boolean) mutator.invoke(instrumentTable, instrumentId)) {
-                throw new NormalizeInstrumentException("Instrument wasn't mutated, probably it was set to default value and FlatBufferBuilder().forceDefaults(false) was used");
-            }
-        } catch (IllegalAccessException | InvocationTargetException e) {
-            throw new NormalizeInstrumentException("Unable to mutate instrument id", e);
-        }
-    }
-
-
-    /**
-     * Constructs original flatbuffers table from {@link FBNormalizeInstrument request
-     *
-     * @param normalizeInstrument wrapper around business message
-     * @return original business message as a flatbuffers table
-     * @throws NormalizeInstrumentException
-     */
-    public static Table getOriginalTable(FBNormalizeInstrument normalizeInstrument) {
-        try {
-            Class<?> clazz = normalizeInstrument.getClass()
-                    .getClassLoader()
-                    .loadClass(normalizeInstrument.originalMessageClass());
-
-            Method rootMaker = clazz.getMethod("getRootAs" + clazz.getSimpleName(), ByteBuffer.class);
-
-            return (Table) rootMaker.invoke(null, normalizeInstrument.originalMessageAsByteBuffer());
-        } catch (ClassNotFoundException | InvocationTargetException | NoSuchMethodException |
-                 IllegalAccessException e) {
-            throw new NormalizeInstrumentException(e);
-        }
+        ByteBuffer byteBuffer = normalizeInstrument.originalMessageAsByteBuffer();
+        byteBuffer.putLong(byteBuffer.position() + normalizeInstrument.instrumentIdOffsest(), instrumentId);
     }
 
     /**
---
+++ communication/src/test/java/com/NormalizeInstrumentTests.java
@@ -0,0 +1,54 @@
+package com;
+
+import com.bnpparibas.gban.communication.messages.internal.instrumentnormalizer.receiver.flatbuffers.FBNormalizeInstrument;
+import com.bnpparibas.gban.communication.messages.internal.instrumentnormalizer.receiver.flatbuffers.FbFixMsg;
+import com.bnpparibas.gban.communication.messages.internal.instrumentnormalizer.receiver.flatbuffers.FbTable1;
+import com.bnpparibas.gban.flatbufferstooling.communication.NormalizeInstrument;
+import com.google.flatbuffers.FlatBufferBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.nio.ByteBuffer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class NormalizeInstrumentTests {
+
+    @Test
+    public void mutateNestedInstrumentId() {
+        FbFixMsg originalMsg = generateFb();
+        FBNormalizeInstrument normalizeInstrument = NormalizeInstrument.wrapWithNormalizeInstrument(originalMsg, 1L, "asd", ".table1", "topic");
+
+        NormalizeInstrument.mutateInstrumentId(normalizeInstrument, Long.MAX_VALUE);
+        byte[] bytes = NormalizeInstrument.cutOriginalMessageFrom(normalizeInstrument);
+
+        FbFixMsg mutatedMsg = FbFixMsg.getRootAsFbFixMsg(ByteBuffer.wrap(bytes));
+        assertEquals(Long.MAX_VALUE, mutatedMsg.table1().instrumentId(), "Wrapped message wasn't mutated");
+    }
+
+    @Test
+    public void mutateInstrumentId() {
+        FbFixMsg originalMsg = generateFb();
+        FBNormalizeInstrument normalizeInstrument = NormalizeInstrument.wrapWithNormalizeInstrument(originalMsg, 1L, "asd", null, "topic");
+
+        NormalizeInstrument.mutateInstrumentId(normalizeInstrument, Long.MAX_VALUE);
+        byte[] bytes = NormalizeInstrument.cutOriginalMessageFrom(normalizeInstrument);
+
+        FbFixMsg mutatedMsg = FbFixMsg.getRootAsFbFixMsg(ByteBuffer.wrap(bytes));
+        assertEquals(Long.MAX_VALUE, mutatedMsg.instrumentId(), "Wrapped message wasn't mutated");
+    }
+
+    private FbFixMsg generateFb() {
+        FlatBufferBuilder flatBufferBuilder = new FlatBufferBuilder().forceDefaults(true);
+
+        int strOff = flatBufferBuilder.createString("abcd");
+
+        int fbTable1 = FbTable1.createFbTable1(flatBufferBuilder, (byte) 3, Long.MIN_VALUE, strOff, (byte) 1, 3L);
+
+        flatBufferBuilder.finish(FbFixMsg.createFbFixMsg(flatBufferBuilder, (byte) 1, fbTable1, Long.MIN_VALUE));
+
+        byte[] bytes = flatBufferBuilder.sizedByteArray();
+        return FbFixMsg.getRootAsFbFixMsg(ByteBuffer.wrap(bytes));
+    }
+
+}
